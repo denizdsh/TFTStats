cc33bddd114f02260018d4f4acf00ddba1f5cdeb
Deniz Shyukryuev <denizztto@gmail.com>
2023-05-01 04:17:22 +0300

    Fix deployment bug

--- src/lib/prebuild.mjs
+++ src/lib/prebuild.mjs
@@ -1,6 +1,7 @@
 // Function needed for fetching and saving locally heavy data at build time.
 
 import path from 'path';
+import { mkdirSync } from 'fs';
 
 import fetchAndSaveFile from './fetchAndSaveFile.mjs';
 import config from '../config.mjs';
@@ -13,6 +14,11 @@ async function mapResponseCallback(res) {
 }
 
 (async () => {
+    // create needed directory, since empty folders are not uplaoded in the repository => deployment bug
+    const directoryPath = path.join(process.cwd(), config.DATA_DIRECTORY);
+    mkdirSync(directoryPath, { recursive: true });
+
+
     const dataUrl = config.CDRAGON_TFT_FILE_URL;
 
     const fileName = config.CDRAGON_TFT_FILENAME
@@ -22,7 +28,9 @@ async function mapResponseCallback(res) {
 
     const filePath = path.join(process.cwd(), config.DATA_DIRECTORY, fileName);
 
+
     console.log(`Fetching file from ${dataUrl}...`);
+
     await fetchAndSaveFile(dataUrl, filePath, mapResponseCallback);
     console.log(`File saved to ${filePath}.`);
 })();
