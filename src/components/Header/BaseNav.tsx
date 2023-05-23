cf9efd3bb19bcc51213efe87d07ddc74d8d6d620
Deniz Shyukryuev <denizztto@gmail.com>
2023-05-23 23:04:03 +0300

    Fix sidebar orintation

--- src/components/Header/BaseNav.tsx
+++ src/components/Header/BaseNav.tsx
@@ -3,43 +3,57 @@ import { usePathname } from "next/navigation";
 import { MouseEventHandler } from "react";
 
 export const baseLinks = [
-    {
-        title: 'Champions',
-        link: '/champions'
-    },
-    {
-        title: 'Summoners',
-        link: '/summoners'
-    },
-    {
-        title: 'Playground',
-        link: '/playground'
-    }
-]
+  {
+    title: "Champions",
+    link: "/champions",
+  },
+  {
+    title: "Summoners",
+    link: "/summoners",
+  },
+  {
+    title: "Playground",
+    link: "/playground",
+  },
+];
 
 interface IBaseNavProps {
-    orientation: 'row' | 'col',
-    hideOn: 'max-md' | 'md',
-    containerClassName?: string,
-    linkClassName?: string,
-    links: { link: string, title: string }[],
-    onLinkClick?: MouseEventHandler
+  orientation: "row" | "col";
+  hideOn: "max-md" | "md";
+  containerClassName?: string;
+  linkClassName?: string;
+  links: { link: string; title: string }[];
+  onLinkClick?: MouseEventHandler;
 }
 
 export default function BaseNav(props: IBaseNavProps) {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-        <nav className={`${props.hideOn === 'md' ? 'md:hidden' : 'max-md:hidden'} col-span-2`}>
-            <ul className={`list-none flex flex-${props.orientation} gap-10 md:max-lg:gap-5 ${props.containerClassName || ''}`}>
-                {props.links.map(l =>
-                    <li key={l.link} className='title-medium'>
-                        <Link href={l.link} onClick={props.onLinkClick} className={`transition-colors hover:text-secondary active:text-tertiary80 focus:text-tertiary80 ${pathname?.startsWith(l.link) ? 'text-tertiary' : ''} ${props.linkClassName || ''}`}>
-                            {l.title}
-                        </Link>
-                    </li>
-                )}
-            </ul>
-        </nav>
-    )
+  return (
+    <nav
+      className={`${
+        props.hideOn === "md" ? "md:hidden" : "max-md:hidden"
+      } col-span-2`}
+    >
+      <ul
+        className={`flex list-none ${
+          props.orientation === "col" ? "flex-col" : ""
+        } gap-10 md:max-lg:gap-5 ${props.containerClassName || ""}`}
+      >
+        {props.links.map((l) => (
+          <li key={l.link} className="title-medium">
+            <Link
+              href={l.link}
+              onClick={props.onLinkClick}
+              className={`transition-colors hover:text-secondary focus:text-tertiary80 active:text-tertiary80 ${
+                pathname?.startsWith(l.link) ? "text-tertiary" : ""
+              } ${props.linkClassName || ""}`}
+            >
+              {l.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
