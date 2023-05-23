394bbdf75d93d7807b87c25c6e8efb89c35bd198
Deniz Shyukryuev <denizztto@gmail.com>
2023-05-23 23:02:01 +0300

    Fix PWA support (next-pwa app directory conflict)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,37 @@
+const path = require('path');
+
+const { withPlaiceholder } = require('@plaiceholder/next');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: isDev,
+  register: true,
+  buildExcludes: ["app-build-manifest.json"],
+})
+
+
+const generateAppDirEntry = (entry) => {
+  const packagePath = require.resolve('next-pwa');
+  const packageDirectory = path.dirname(packagePath);
+  const registerJs = path.join(packageDirectory, "register.js");
+
+  return entry().then((entries) => {
+    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+      if (Array.isArray(entries["main-app"])) {
+        entries["main-app"].unshift(registerJs);
+      } else if (typeof entries["main-app"] === "string") {
+        entries["main-app"] = [registerJs, entries["main-app"]];
+      }
+    }
+    return entries;
+  });
+};
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -11,13 +45,18 @@ const nextConfig = {
         hostname: '**.communitydragon.org',
       }
     ]
+  },
+  webpack: (config) => {
+    const entry = generateAppDirEntry(config.entry);
+    config.entry = () => entry;
+
+    return config;
   }
 }
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true
-})
 
-module.exports = withPWA(nextConfig);
+module.exports = withPWA(
+  withPlaiceholder(
+    nextConfig
+  )
+);
